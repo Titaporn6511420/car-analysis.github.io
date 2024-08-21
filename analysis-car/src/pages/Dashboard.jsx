953abc9c046a660732593d56b095bc6e81a5a3b1
Sirinalin <u6510240@au.edu>
2024-08-21 21:55:25 +0700

    mint 1
    page can change

---
+++ analysis-car/src/pages/Dashboard.jsx
@@ -0,0 +1,13 @@
+// src/pages/Dashboard.jsx
+import React from 'react';
+
+function Dashboard() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      {/* Add your dashboard content, charts, and tables here */}
+    </div>
+  );
+}
+
+export default Dashboard;
---
+++ analysis-car/src/pages/HighlightedCars.jsx
@@ -0,0 +1,13 @@
+// src/pages/HighlightedCars.jsx
+import React from 'react';
+
+function HighlightedCars() {
+  return (
+    <div>
+      <h1>Highlighted Cars</h1>
+      {/* Add your highlighted cars content here */}
+    </div>
+  );
+}
+
+export default HighlightedCars;
